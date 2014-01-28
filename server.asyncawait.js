085cda571ac6c023abf22d14fcd83c5dbcc4c848
Luke Hoban <lukehoban@gmail.com>
2014-01-28 15:27:42 -0800

    Cleanup

--- server.asyncawait.js
+++ server.asyncawait.js
@@ -3,34 +3,34 @@
 macro function! {
   case {_ $name ($params ...) { $body ...} } => {
     return #{ var $name = require('q').async(function * $name ($params ...) { $body ... }) }
-    }
+  }
   case {_ ($params ...) { $body ...} } => {
     return #{ require('q').async(function * ($params ...) { $body ... }) }
-    }
+  }
 }
 macro await {
   case {_ $e:expr } => {
     return #{ yield $e }
-    }
-    }
+  }
+}
 
 let var = macro {
-    rule { $name:ident = $value:expr } => {
-        var $name = $value
-    }
+  rule { $name:ident = $value:expr } => {
+    var $name = $value
+  }
  
-    rule { {$name:ident (,) ...} = $value:expr } => {
-        var object = $value
-        $(, $name = object.$name) ...
-    }
+  rule { {$name:ident (,) ...} = $value:expr } => {
+    var object = $value
+    $(, $name = object.$name) ...
+  }
  
-    rule { [$name:ident (,) ...] = $value:expr } => {
-        var array = $value, index = 0
-        $(, $name = array[index++]) ...
-    }
-    }
+  rule { [$name:ident (,) ...] = $value:expr } => {
+    var array = $value, index = 0
+    $(, $name = array[index++]) ...
+  }
+}
 
-    // app ------------------------------------------------
+// app ------------------------------------------------
 
 var http = require('http');
 var Q = require('q');
@@ -38,20 +38,20 @@ var request = require('./request.js');
 var headers = { 'User-Agent': 'lukehoban', 'Authorization': 'token 665021d813ad67942206d94c47d7947716d27f66' };
 
 function! getCollaboratorImages(full_name) {
-    // promise-returning async HTTP GET
-    // note - if any exceptions are thrown here they will propogate into try/catch in callers
-    var url = 'https://api.github.com/repos/' + full_name + '/collaborators';
-    var [response, body] = await request({url: url, headers: headers}); 
-    console.log('got a collab response:' + url + ': ' + JSON.stringify(response.headers));
-    return JSON.parse(body).map(function(collab) {
-        console.log('got an avatar');
-        return collab.avatar_url;
-    });;
+  // promise-returning async HTTP GET
+  // note - if any exceptions are thrown here they will propogate into try/catch in callers
+  var url = 'https://api.github.com/repos/' + full_name + '/collaborators';
+  var [response, body] = await request({url: url, headers: headers}); 
+  console.log('got a collab response:' + url + ': ' + JSON.stringify(response.headers));
+  return JSON.parse(body).map(function(collab) {
+    console.log('got an avatar');
+    return collab.avatar_url;
+  });;
 }
 
 // can use a function! here because createServer doesn't care what this returns
 http.createServer(function! (req, res) {
-    console.log('starting...')
+  console.log('starting...')
   var url = 'https://api.github.com/search/repositories?per_page=100&q=' + 'tetris';
   var items = [];
   // write a normal 'synchronous' while loop
@@ -72,7 +72,7 @@ http.createServer(function! (req, res) {
       url = (/<(.*)>; rel="next"/.exec(response.headers.link) || [])[1];
       // break once there is no 'next' link
       if(!url) break; 
-  } catch(err) {
+    } catch(err) {
       console.log('backing off... ' + err)
       // backoff on any error
       await Q.timeout(1000); 
